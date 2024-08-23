611e22bdc7a6fc62ca64a188a5eb2f209651d8fb
MoRan <ooklibaioo@gmail.com>
2024-08-23 17:50:27 +0800

    Update KLWP Detail Page

--- public/js/script.js
+++ public/js/script.js
@@ -75,3 +75,14 @@ function signupValidateForm() {
 function goBack() {
     window.history.back();
 }
+
+function loadPage(route, loadTarget, TargetEle) {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById(TargetEle).innerHTML = loadTarget
+        }
+    }
+    xhttp.open("GET", "/" + route, true)
+    xhttp.send()
+}
